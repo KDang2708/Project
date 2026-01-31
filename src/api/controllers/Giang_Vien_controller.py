4fab762cac7793e56956bc86740a4104b532af5f
TlxLeTinNghia <letinnghia1582006@hia1582006gmail.82006gmailcom>
2026-01-31 16:32:04 +0700

    Xem_nhom

--- src/api/controllers/Giang_Vien_controller.py
+++ src/api/controllers/Giang_Vien_controller.py
@@ -6,11 +6,15 @@ from services.Phan_Nhom import PhanNhomUseCase
 from api.schemas.requests.Tao_Nhom import TaoNhomRequest
 from api.schemas.responses.Tao_Nhom import TaoNhomResponse
 from api.schemas.requests.Them_Sinh_Vien_Nhom import ThemSinhVienNhomRequest
+from services.Xem_Nhom import XemNhomUseCase
+from api.schemas.requests.Xem_Nhom import XemNhomRequest
+from api.schemas.responses.Xem_Nhom import XemNhomResponse , SinhVien
 
 class GiangVienController:
-    def __init__(self , xem_lop_hoc : XemLopHocUseCase , phan_nhom : PhanNhomUseCase):
+    def __init__(self , xem_lop_hoc : XemLopHocUseCase , phan_nhom : PhanNhomUseCase , xem_nhom : XemNhomUseCase):
         self.ser_xem_lop_hoc = xem_lop_hoc
         self.ser_phan_nhom = phan_nhom
+        self.ser_xem_nhom = xem_nhom
 
     def xem_lop_hoc(self)->list[XemLopHocResponse]:
         try:
@@ -40,6 +44,24 @@ class GiangVienController:
     def them_sinh_vien_vao_nhom(self, request : ThemSinhVienNhomRequest):
         try:
             self.ser_phan_nhom.them_sinh_vien(request.id_sinh_vien , request.id_nhom)
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(e)
+            )
+    def xem_nhom(self , request : XemNhomRequest)->XemNhomResponse:
+        try:
+            nhom = self.ser_xem_nhom.execute(request.id_nhom)
+            return XemNhomResponse(
+                id_nhom=nhom.id,
+                danh_sach_sinh_vien = [
+                    SinhVien(
+                        id_sinh_vien=SV.id,
+                        ten_sinh_vien=SV.ten
+                    )
+                    for SV in nhom.danh_sach_hoc_sinh
+                ] 
+            )
         except ValueError as e:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
--- src/api/routes/Giang_Vien_route.py
+++ src/api/routes/Giang_Vien_route.py
@@ -8,13 +8,17 @@ from api.schemas.responses.Tao_Nhom import TaoNhomResponse
 from api.schemas.requests.Tao_Nhom import TaoNhomRequest
 from services.Phan_Nhom import PhanNhomUseCase
 from api.schemas.requests.Them_Sinh_Vien_Nhom import ThemSinhVienNhomRequest
+from services.Xem_Nhom import XemNhomUseCase
+from api.schemas.responses.Xem_Nhom import XemNhomResponse
+from api.schemas.requests.Xem_Nhom import XemNhomRequest
 
 router = APIRouter(prefix="/giang_vien",tags=["GiangVien"])
 
 def get_giang_vien_controller():
     return GiangVienController(
         xem_lop_hoc = XemLopHocUseCase(),
-        phan_nhom= PhanNhomUseCase()
+        phan_nhom= PhanNhomUseCase(),
+        xem_nhom= XemNhomUseCase(),
     )
 
 @router.get("/xem_lop_hoc", response_model=list[XemLopHocResponse],status_code=200)
@@ -37,3 +41,10 @@ def them_sinh_vien_nhom(
     controller : GiangVienController = Depends(get_giang_vien_controller)
 ):
     return controller.them_sinh_vien_vao_nhom(request)
+@router.get("/xem_nhom",response_model=XemNhomResponse,status_code=200)
+def xem_nhom(
+    request : XemNhomRequest,
+    user = Depends(get_current_user),
+    controller : GiangVienController = Depends(get_giang_vien_controller)
+):
+    return controller.xem_nhom(request)
